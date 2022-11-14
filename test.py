fa916ba2f0215a432d6988aaa5387e6bbc9e9a56
jiwon-center <https://github.com/jiwon-center>
2022-11-14 21:26:42 +0900

    try

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ conn = pymysql.connect(host=os.environ.get('DB_URL'),
                        db=os.environ.get('DB_NAME'),
                        charset='utf8')
 
-sql = "INSERT INTO user (user_name, user_student_number, user_major, user_login_time) VALUES (%s, %s, %s, %s)"
+sql = "INSERT INTO user (user_name, user_student_number, user_major, user_login_time, user_token) VALUES (%s, %s, %s, %s, %s)"
 like_button = 0
 
 def template(contents, content):
@@ -89,8 +89,8 @@ def getUserID(user_token):
                 WHERE user_token = {user_token}
                 """
     with conn.cursor() as cur:
-            cur.execute(getUsID)
-            user_data = cur.fetchall()
+        cur.execute(getUsID)
+        user_data = cur.fetchall()
     return user_data[0][0]
 
 def getClassContents(class_code):
@@ -134,24 +134,24 @@ def login():
         User_name = request.form['User_name']
         User_major = request.form['User_major']
         User_login_time = datetime.datetime.now()
+        User_token = secrets.token_hex(nbytes=32)
+        session['User_token'] = User_token
         session['User_name'] = request.form['User_name']
         
         with conn.cursor() as cur:
-            cur.execute(sql, (User_name, Student_ID, User_major, User_login_time))
+            cur.execute(sql, (User_name, Student_ID, User_major, User_login_time, User_token))
         conn.commit()
         
-        User_token = secrets.token_hex(nbytes=32)
-        sql2 = f"""SELECT user_id FROM user WHERE user_name = '{User_name}'"""
+        
+        sql2 = f"""SELECT user_id FROM user WHERE user_token = '{User_token}'"""
 
         with conn.cursor() as cur:
             cur.execute(sql2)
         user_id_db_result = cur.fetchall()
-
-        print(user_id_db_result[0][0])
-
-        session['User_token'] = User_token
+        session['User_id'] = user_id_db_result[0][0]
         print(session['User_token'])
         print(session['User_name'])
+        print(session['User_id'])
         
         return redirect(url_for('index'))
         
@@ -217,7 +217,6 @@ def project(id):
 @app.route('/session-check', methods=['POST'])
 def session_check():
     print(session)
-
     session_check_json = request.get_json()
 
     if session_check_json['token'] in session:
@@ -227,18 +226,68 @@ def session_check():
 
 @app.route('/project/<int:pj_id>/like')
 def likes_project(pj_id):
-    us_id = getUserID(session['User_token'])
+    us_id = session['User_id']
     conn = pymysql.connect(host=os.environ.get('DB_URL'),
                        user=os.environ.get('DB_USER'),
                        password=os.environ.get('DB_PASSWORD'),
                        db=os.environ.get('DB_NAME'),
                        charset='utf8')
-    likesql = f"""SELECT EXISTS(SELECT * FROM like_table WHERE project_id == {pj_id} AND user_id == {us_id}) AS t"""
-    conn.cursor.execute(likesql)
-    like_button = conn.cursor.fetchall()
-    like_button = like_button[0][0]
-    print(like_button)
-    return like_button
+    likesql = f"""SELECT EXISTS(SELECT * FROM like_table WHERE project_id = {pj_id} AND user_id = {us_id}) AS t"""
+    with conn.cursor() as cur:
+        cur.execute(likesql)
+        like_button = cur.fetchall()
+        like_button = like_button[0][0]
+        conn.commit()
+        
+    if like_button == 0:
+        likeup= f"""
+                UPDATE project
+                set like_cnt = like_cnt + 1
+                where project_id = {pj_id}
+                """
+        liketable= f"""
+                   INSERT into like_table
+                   (user_id, project_id) VALUES
+                   ({us_id}, {pj_id})
+                   """
+        likesql = f"""SELECT EXISTS(
+                   SELECT * FROM like_table
+                   WHERE user_id = {us_id} AND
+                   project_id = {pj_id}) AS t"""
+    
+        with conn.cursor() as cur:
+            cur.execute(likeup)
+            cur.execute(likesql)
+            cur.execute(liketable)
+            like_data = cur.fetchall()
+            conn.commit()
+        print(like_data[0][0], '\n') 
+        print(session['User_name'])
+        print('님이 좋아요를 눌렀어요')   
+        return jsonify({'msg': '좋아요 완료!'})
+    
+    else :
+        likeup= f"""
+                UPDATE project
+                set like_cnt = like_cnt - 1
+                where project_id = {pj_id}
+                """
+        liketable= f"""
+                   DELETE table like_table WHERE user_id = {us_id} AND project_id = {pj_id})
+                   """
+        likecnts = f"""
+                   SELECT like_cnt
+                   FROM project
+                   where project_id = {pj_id}
+                   """
+        with conn.cursor() as cur:
+            cur.execute(likeup)
+            cur.execute(likecnts)
+            cur.execute(liketable)
+            like_data = cur.fetchall()
+        print(like_data)    
+        like_button = 0
+        return jsonify({'msg': '좋아요 취소!'})
 
 @app.route('/projects/<int:pj_id>/like')
 def like_project(pj_id):
@@ -254,9 +303,14 @@ def like_project(pj_id):
                    FROM project
                    where project_id = {pj_id}
                    """
+        likesql = f"""SELECT EXISTS(
+                   SELECT * FROM like_table
+                   WHERE project_id = {pj_id}
+                   AND user_id = 1) AS t"""
+    
         with conn.cursor() as cur:
             cur.execute(likeup)
-            cur.execute(likecnts)
+            cur.execute(likesql)
             like_data = cur.fetchall()
         print(like_data[0][0], '\n') 
         print(session['User_name'])
