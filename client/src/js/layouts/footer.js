23e57313da22c064d2545b0fc722c101a881e3e2
nimod7890 <nimod1234@g.skku.edu>
2022-11-22 21:20:39 +0900

    fe: footer

--- client/src/js/layouts/footer.js
+++ client/src/js/layouts/footer.js
@@ -5,7 +5,14 @@ const Footer = () => {
     <footer>
       <div className="footerWrapper">
         <div className="copyright">
-          <p>Copyright ⓒ 2022 Learning Fair, Sungkyunkwan University <br/>데이터사이언스교육센터(dsedu.skku.edu)</p>
+          <p>
+            Copyright ⓒ 2022 Learning Fair, Sungkyunkwan University <br />
+            데이터사이언스교육센터(
+            <a target="_blank" href="http://dsedu.skku.edu" rel="noreferrer">
+              dsedu.skku.edu
+            </a>
+            )
+          </p>
         </div>
       </div>
     </footer>
